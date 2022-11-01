85dd56e7be307fa386ffaafdaab1b7b2facde35f
lucascr91 <lucas.ecomg@gmail.com>
2022-11-01 12:05:43 -0300

    add db service

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+'''
+Calculator functions
+'''
+
+
+def add(x,y):
+    '''Add two numbers'''
+    return x + y
+
+def subtract(x,y):
+    '''Subtract x from y and return result'''
+
+    return y-x
